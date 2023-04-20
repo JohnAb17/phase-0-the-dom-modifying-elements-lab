2bd00487adb97e7648bc499b75fee4c66a2a05ba
John Medina <john9713medina@hotmail.com>
2023-04-20 02:16:12 -0700

    Donezo

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code here!
+main.remove();
+const newHeader = document.createElement("h1");
+//document.body.append(newHeader);
+
+newHeader.id = 'victory';
+//document.body.append(newHeader);
+//newHeader.textContent = "victory";
+
+
+
+newHeader.textContent = "John is the champion";
+//document.body.append(textContent);
+document.body.append(newHeader);
